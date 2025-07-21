af75abbf6ab16accfee74c7e40cd222ca37f423d
hardi25vekariya <24it108@charusat.edu.in>
2025-07-21 21:37:11 +0530

    Create UseInputOutput.java

---
+++ UseInputOutput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class UserInputOutput
+{
+  public static void main(String[],args)
+  {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the int.. number:");
+      int n=sc.nextint();
+      System.out.println(n);
+  }
+}
+  
